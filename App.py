bcf2c259ace24b7efacf6fbdfd253e03aee6ffbb
Schwaxpl <schwaxpl+git@gmail.com>
2024-02-15 15:33:51 +0100

    Upload du csv et import dataframe

--- App.py
+++ App.py
@@ -4,20 +4,8 @@ import numpy as np
 import Pages as p
 st.title('Projet streamlit groupe 3')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+if("data" in st.session_state):
+    data = st.session_state["data"]
+    st.dataframe(data)
+else:
+    st.text("Bienvenue, allez dans file upload pour charger un CSV")
