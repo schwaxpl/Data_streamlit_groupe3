7a46f6a45f65e6eabd41f8617dfb95dec560de4b
LMBorderon <borderon.lm@gmail.com>
2024-02-19 15:41:04 +0100

    supp import ydata-profile - Nettoyage &  Encodage

--- pages/Nettoyage.py
+++ pages/Nettoyage.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from ydata_profiling import ProfileReport
 import missingno as msno
 import matplotlib.pyplot as plt
 import streamlit_extras
