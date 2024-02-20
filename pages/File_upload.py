0d24409f648ac9b0a21a1d75d7cd41bb141dd653
Schwaxpl <schwaxpl+git@gmail.com>
2024-02-20 14:56:17 +0100

    séparateur csv

--- pages/File_upload.py
+++ pages/File_upload.py
@@ -5,9 +5,12 @@ import Utils.Utils as u
 u.init_page("File_upload")
 
 file = st.file_uploader(label="Mettez votre CSV",type="csv",accept_multiple_files=False)
-
+txt_sep = st.text_input("Séparateur",",",max_chars=3)
 if file:
-    data = pd.read_csv(file)
+    sep = ","
+    if (txt_sep != ""):
+        sep = txt_sep 
+    data = pd.read_csv(file,sep=sep)
     st.success("✅ Votre fichier " + str(file.name) + " a été correctement importé. Voici un aperçu des données :" )
     st.dataframe(data.head())
     st.session_state["data"] = data
