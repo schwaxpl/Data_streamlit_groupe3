2e35ea827c18a0aab5c4a8a92582e8efa4cf60a2
Schwaxpl <schwaxpl+git@gmail.com>
2024-02-21 00:07:48 +0100

    Pagination, model training, reset à l'upload

--- pages/File_upload.py
+++ pages/File_upload.py
@@ -11,7 +11,11 @@ if file:
     if (txt_sep != ""):
         sep = txt_sep 
     data = pd.read_csv(file,sep=sep)
-    st.success("✅ Votre fichier " + str(file.name) + " a été correctement importé. Voici un aperçu des données :" )
+    if("model" in st.session_state):
+        del st.session_state["model"]
+    if("train_test" in st.session_state):
+        del st.session_state["train_test"]
+    st.success("Votre fichier " + str(file.name) + " a été correctement importé. Voici un aperçu des données :",icon="✅" )
     st.dataframe(data.head())
     st.session_state["data"] = data
     st.session_state["nom_dataset"] = str(file.name).split('.')[-2]
